fd914db0f92abf7457ca61b745bd24fa03d62dd1
Aryaman Saxena <135634505+WITCHER004@users.noreply.github.com>
2023-10-03 23:26:53 +0530

    Add files via upload
    THIS CODE ASSISTS IN CALCULATING AN INDIVIDUAL'S BODY MASS INDEX BASED ON HIS/HER HEIGHT (IN METRES )AND WEIGHT (IN KILOGRAMMES).

---
+++ CODE.py
@@ -0,0 +1,7 @@
+#BODY MASS INDEX CALCULATOR:
+n=input("ENTER YOUR NAME:")
+h=eval(input("ENTER YOUR HEIGHT:"))
+i=h * 0.348
+w=eval(input("ENTER YOUR WEIGHT:"))
+bmi=w/(i**2)
+print("THE BODY MASS INDEX OF",n,"IS",bmi)
