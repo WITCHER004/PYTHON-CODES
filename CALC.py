8105c78f3020e9fbe8c666b22189b85b13c5902d
Aryaman Saxena <135634505+WITCHER004@users.noreply.github.com>
2023-10-04 23:39:01 +0530

    Update CALC.py

--- CALC.py
+++ CALC.py
@@ -5,17 +5,17 @@ print('''1.ADDITION
          4.MULTIPLICATION
          5.SQUARING''')
 n=eval(input("ENTER THE OPERATION YOU WANT EXECUTE:"))
-if(n==1):
+if(n == 1):
     a=eval(input("ENTER THE INTEGER :"))
     b=eval(input("ENTER THE INTEGER :"))
     c=a+b
     print("THE SUM IS",c)
-elif(n==2):
+elif(n == 2):
     a=eval(input("ENTER THE INTEGER :"))
     b=eval(input("ENTER THE INTEGER :"))
     c=a-b
     print("THE DIFFERENCE IS",c)
-elif (n ==3):
+elif (n == 3):
     a=eval(input("ENTER THE INTEGER :"))
     b=eval(input("ENTER THE INTEGER :"))
     if b != 0:
@@ -23,14 +23,12 @@ elif (n ==3):
         print("THE QUOTIENT IS",result)
     else:
         print("Division by zero is not allowed.")
-   
-
-elif(n==4):
+elif(n == 4):
     a=eval(input("ENTER THE INTEGER :"))
     b=eval(input("ENTER THE INTEGER :"))
     c=a*b
     print("THE PRODUCT IS",c)
-elif(n==5):
+elif(n == 5):
     a=int(input("ENTER THE INTEGER :"))
     b=a**2
     print("THE SQUARE IS",b)
