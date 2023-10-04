5efe82f91b3a072ceab5df92cabdc562382869e9
Aryaman Saxena <135634505+WITCHER004@users.noreply.github.com>
2023-10-04 23:07:25 +0530

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -1,7 +1,14 @@
-#BODY MASS INDEX CALCULATOR:
+#BODY MASS INDEX CALCULATOR:-
+
 n=input("ENTER YOUR NAME:")
 h=eval(input("ENTER YOUR HEIGHT:"))
 i=h * 0.348
 w=eval(input("ENTER YOUR WEIGHT:"))
 bmi=w/(i**2)
+if(bmi >= 25):
+  print("THE USER FALLS IN OBESITY RANGE")
+elif(bmi >= 18 and bmi < 25):
+  print("THE USER IS FIT ")
+else:
+  print("THE USER IS UNDERWEIGHT")
 print("THE BODY MASS INDEX OF",n,"IS",bmi)
